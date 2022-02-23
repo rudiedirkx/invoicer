2fb759209a076499c05b2f119ef67ead353fd00c
Rudie Dirkx <github@hotblocks.nl>
2022-02-23 20:51:44 +0100

    vscroll tables

--- tpl.invoices.php
+++ tpl.invoices.php
@@ -6,27 +6,29 @@ Invoice::eagers($invoices, ['num_lines', 'total_subtotal']);
 
 ?>
 
-<table>
-	<thead>
-		<tr>
-			<th>Number</th>
-			<? if ($show_client ?? false): ?>
-				<th>Client</th>
-			<? endif ?>
-			<th>Subject</th>
-			<th>Summary</th>
-		</tr>
-	</thead>
-	<tbody>
-		<? foreach ($invoices as $invoice): ?>
+<div class="vscroll">
+	<table>
+		<thead>
 			<tr>
-				<td><a href="<?= get_url('invoice', ['id' => $invoice->id]) ?>"><?= html($invoice->number_full) ?></a></td>
+				<th>Number</th>
 				<? if ($show_client ?? false): ?>
-					<td><?= html($invoice->client) ?></td>
+					<th>Client</th>
 				<? endif ?>
-				<td><?= html($invoice->description) ?></td>
-				<td><?= html($invoice->summary) ?></td>
+				<th>Subject</th>
+				<th>Summary</th>
 			</tr>
-		<? endforeach ?>
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			<? foreach ($invoices as $invoice): ?>
+				<tr>
+					<td><a href="<?= get_url('invoice', ['id' => $invoice->id]) ?>"><?= html($invoice->number_full) ?></a></td>
+					<? if ($show_client ?? false): ?>
+						<td><?= html($invoice->client) ?></td>
+					<? endif ?>
+					<td><?= html($invoice->description) ?></td>
+					<td><?= html($invoice->summary) ?></td>
+				</tr>
+			<? endforeach ?>
+		</tbody>
+	</table>
+</div>

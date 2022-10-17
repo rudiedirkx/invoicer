f90f180135c99b741a2fde684a1771db61fe4814
Rudie Dirkx <github@hotblocks.nl>
2022-10-17 13:43:15 +0200

    favicon+

--- tpl.header.php
+++ tpl.header.php
@@ -7,6 +7,9 @@ header('Content-type: text/html; charset=utf-8');
 <html lang="en">
 <title>Invoicer</title>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 <meta name="theme-color" content="#333" />
 <meta charset="utf-8" />
 <meta name="referrer" content="no-referrer" />
